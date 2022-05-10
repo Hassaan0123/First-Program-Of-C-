8f6693e449758b63f2bd806dcdeac240073d5e76
Hassaan Ahmed <102408829+Hassaan0123@users.noreply.github.com>
2022-05-10 18:20:17 +0500

    Add files via upload

---
+++ File1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout <<"Hello world!"<<endl;
+    
+    cout <<"Enter Any Number: ";
+    cin>>num;
+    cout<<"The number you have entered is: "<<num;
+    
+    return 0;
+}
